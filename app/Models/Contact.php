1f84a799d443f526a5bebd658a09687a05bb5230
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-04-27 13:45:59 +0600

    feat: Add relationships for event and user in Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
