f3aa6f70fc33403f74aba7154a6585c5d04aaedc
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-04-27 13:40:36 +0600

    feat: Add relationships for fest, registrationLogs, registrationQuestionAnswers, and registrationQuestions in Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    public function fest()
+    {
+        return $this->belongsTo(Fest::class);
+    }
+
+    public function registrationLogs()
+    {
+        return $this->hasMany(RegistrationLog::class);
+    }
+
+    public function registrationQuestionAnswers()
+    {
+        return $this->hasMany(RegistrationQuestionAnswer::class);
+    }
+
+    public function registrationQuestions()
+    {
+        return $this->hasMany(RegistrationQuestionField::class);
+    }
 }
