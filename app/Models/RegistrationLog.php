4f215915ec528b1f04d5e7f24e044c3953e14701
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-04-27 13:43:03 +0600

    feat: Add user and event relationships in RegistrationLog model

--- app/Models/RegistrationLog.php
+++ app/Models/RegistrationLog.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class RegistrationLog extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
 }
