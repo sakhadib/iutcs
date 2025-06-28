bbfe47294cb52e55937d8ad7d3628b9d7498eec8
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-06-28 16:26:07 +0600

    feat: Enhance fest details page by including associated event images

--- app/Http/Controllers/FestsController.php
+++ app/Http/Controllers/FestsController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Fest;
-use App\Models\Event;   
+use App\Models\Event;
+use App\Models\EventImage;
 
 class FestsController extends Controller
 {
     public function showFestsPage()
     {
         $ongoing_fest = Fest::orderBy('created_at', 'desc')
-                            ->where('end_date', '>=', now())
                             ->withCount('events')
                             ->first();
         
@@ -46,10 +46,17 @@ class FestsController extends Controller
                         'min_team_size',
                         'image'
                     ]);
+
+        $event_ids = $events->pluck('id')->toArray();
+
+        $fest_images = EventImage::whereIn('event_id', $event_ids)
+                        ->orderBy('created_at', 'desc')
+                        ->get();
         
         return view('frontend.fest-details',[
             'fest' => $fest,
-            'events' => $events
+            'events' => $events,
+            'fest_images' => $fest_images
         ]);
     }
 }
